ca3e145ce90588159e0f78237b27e8be01293ef6
shorekeeper <quownxhutw@gmail.com>
2025-11-23 17:59:26 +0300

    Installer packages placeholder

---
+++ packages/PKGLIST.lua
@@ -0,0 +1,24 @@
+-- Axis OS Package Manifest
+return {
+    { 
+        name = "Drivers", 
+        items = {
+            { name = "s.txt", path = "packages/drivers/s.txt", type = "file" },
+            { name = "Experimental", type = "tree", items = {
+                { name = "holo.lua", path = "packages/drivers/experimental/s.txt", type = "file" },
+                { name = "tape.lua", path = "packages/drivers/experimental/s.txt", type = "file" }
+            }},
+        }
+    },
+    { 
+        name = "Executable", 
+        items = {
+            { name = "s.txt", path = "packages/executable/s.txt", type = "file" },
+            { name = "Utils", type = "tree", items = {
+                 { name = "s.txt", path = "packages/executable/experimental/s.txt", type = "file" }
+            }}
+        }
+    },
+    { name = "Modules", items = {} },
+    { name = "Multilib", items = {} }
+}
